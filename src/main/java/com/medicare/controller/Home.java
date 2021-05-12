adc36465ea02eeb4fceb13654f077267b7f5ddd2
AjayBisht19 <ajayb381@gmail.com>
2021-05-12 17:47:10 +0530

    Authentication using JWT

---
+++ src/main/java/com/medicare/controller/Home.java
@@ -0,0 +1,84 @@
+package com.medicare.controller;
+
+import java.util.List;
+
+//import java.util.Random; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+//import com.medicare.dto.EmailRequest;
+import com.medicare.dto.UserRequest;
+import com.medicare.model.User;
+import com.medicare.repo.UserRepository;
+import com.medicare.service.AuthService;
+//import com.medicare.service.EmailService;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class Home {
+
+	@Autowired
+	private AuthService authService;
+
+	@Autowired
+	private UserRepository userRepository;
+
+//	@Autowired
+//	private EmailService emailService;
+
+	@PostMapping("/signup")
+	public ResponseEntity<?> signup(@RequestBody UserRequest userRequest) {
+		User user = this.authService.signup(userRequest);
+		return ResponseEntity.ok(user);
+	}
+
+	@GetMapping("/checkUsername/{username}")
+	public ResponseEntity<?> checkUsername(@PathVariable("username") String username) {
+		System.out.println("askjdhfkjashdkfjhaksdhfljsdh");
+		User user = this.userRepository.findByUsername(username);
+		System.out.println(user);
+		if (user == null) {
+			return ResponseEntity.ok(false);
+		}
+		return ResponseEntity.ok(true);
+	}
+
+//	@PostMapping("/email")
+//	public String sendEmail(@RequestBody EmailRequest emailRequest) {
+//		Random random = new Random(100000);
+//		int num = random.nextInt(999999);
+//		String otp = Integer.toString(num);
+//		String message="Your valid One Time Password for Medicare account is "+ otp;
+//		
+//		this.emailService.sendEmail(message, emailRequest.getTo());
+//		System.out.println(otp+ " OTP");
+//		
+//		return otp;
+//	}
+
+	@GetMapping("/welcome")
+	public String home() {
+		String text = "This page is nont allowed for unauthorized users";
+		return text;
+	}
+
+	@GetMapping("user/welcome")
+	public String user() {
+		String text = "User only";
+		return text;
+	}
+
+	@GetMapping("admin/welcome")
+	public String admin() {
+		String text = "Admin only";
+		return text;
+	}
+}
---
+++ src/main/java/com/medicare/model/User.java
@@ -0,0 +1,30 @@
+package com.medicare.model;
+
+import javax.persistence.Entity; 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	
+	private String email;
+	private String username;
+	private String password;
+	private String name;
+	private String role;
+	private Boolean isEnabled=false;
+	private String address;
+
+}
