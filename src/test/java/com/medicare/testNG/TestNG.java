8210d3b16beb49f1e9d3cdf5b0e6345a3286850b
AjayBisht19 <ajayb381@gmail.com>
2021-06-03 12:37:59 +0530

    first selenium test

--- src/test/java/com/medicare/testNG/TestNG.java
+++ src/test/java/com/medicare/testNG/TestNG.java
@@ -1,11 +1,31 @@
 package com.medicare.testNG;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
 
 public class TestNG {
 
 	@Test
-	public void confirm() {
-		System.out.println("inside test branch confirmation");
+	public void chrome() {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.simplilearn.com/");
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+	}
+	
+	@Test
+	public void edge() {
+		System.setProperty("webdriver.edge.driver", "msedgedriver.exe");
+		WebDriver driver1 = new EdgeDriver();
+		driver1.get("https://www.google.com/");
+
+		driver1.manage().window().maximize();
+		driver1.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
 	}
 }
